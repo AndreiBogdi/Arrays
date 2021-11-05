7444df2219616fd743edc86b3deca815693989e3
Andrei Nica <andreinyca@gmail.com>
2021-11-05 20:47:43 +0200

    TemaIf

--- src/Lab3.java
+++ src/Lab3.java
@@ -7,23 +7,35 @@ public class Lab3 {
         int number = 8;
         int x = 5;
         int y = 8;
+        int age = 18;
+        int v = 18;
+        int a = 10;
+        int b = 25;
+        int c = 42;
+        int numarpar = 3;
         LogicalOperation op = new LogicalOperation();
+        int commparatie = op.comparasion(x,y);
         Calculator calc = new Calculator();
         double adunare = calc.sum(x,y);
         double scadere = calc.minus(x,y);
         int inmultire = calc.multiplication(x,y);
         float impartire = calc.division(x,y);
-        int commparatie = op.comparasion(x,y);
-        System.out.println(op.verifyProvidedText(text,text2,text3));
-        System.out.println(op.getNumber(number));
-        System.out.println("Rezultatul adunarii 5+8 este "+ (adunare));
+        String numar = op.isNumberEven(numarpar);
+        int BiggestNumber = op.BiggestNumber(a, b, c);
+
+
+        System.out.println("Exercitiul 1: \n Rezultatul adunarii 5+8 este "+ (adunare));
         System.out.println("Rezultatul scaderii 5-8 este "+ (scadere));
         System.out.println("Rezultatul inmultirii 5*8 este "+ (inmultire));
         System.out.println("Rezultatul adunarii 5:8 este "+ (impartire));
-        System.out.println("Dintre 5 si 8 mai mare este "+ (commparatie));
+        System.out.println("Exercitiul 3: Dintre 5 si 8 mai mare este "+ (commparatie));
+        System.out.println("Exercitiul 4: "+(op.verifyProvidedText(text,text2,text3)));
         System.out.println("Exercitiul 5: " + (op.verifyText(text4,number)));
         System.out.println("Exercitiul 6: " +op.numarcomparatie(number));
         System.out.println("Exercitiul 7 " +op.numere3si4(number));
-        System.out.println("Exercitiul 8");
+        System.out.println("Exercitiul 8:The number is: " +(op.getNumber(number))+"!");
+        System.out.println("Exercitiul 9:Andrei " + (op.isEligibleToVote(17)) +" drept de vot!");
+        System.out.println("Exercitiul 10: " +(numarpar) +" este numar " +(numar));
+        System.out.println("Exercitiul 11: Cel mai mare numar este " +(BiggestNumber));
     }
 }
--- src/LogicalOperation.java
+++ src/LogicalOperation.java
@@ -14,39 +14,70 @@ public class LogicalOperation {
         }
         return number;
     }
-    public int comparasion(int first, int second){
-        if (first > second){
+
+    public int comparasion(int first, int second) {
+        if (first > second) {
             return first;
-        } else{
+        } else {
             return second;
         }
 
     }
-    public String verifyText(String text4, int number){
-        if(text4.equals("FastTrack") && number <=3){
+
+    public String verifyText(String text4, int number) {
+        if (text4.equals("FastTrack") && number <= 3) {
             return text4 + number;
-        } else if (!text4.equals("FastTrack") && number >=4){
+        } else if (!text4.equals("FastTrack") && number >= 4) {
             return number + text4;
         }
-        return "Not Found!";
+        return "Not found";
     }
-    public String numarcomparatie(int number){
-        if(number >8 || number==6){
+
+    public String numarcomparatie(int number) {
+        if (number > 8 || number == 6) {
             return "The amount of snow this winter was(cm):" + number;
-        }else {
-            return "The forecast snow is: "+ number;
+        } else {
+            return "The forecast snow is: " + number;
         }
     }
-    public String numere3si4(int number){
-        if(number >3 && number!=4){
+
+    public String numere3si4(int number) {
+        if (number > 3 && number != 4) {
             return "The number is greater than 3 and not equal to 4.";
-        }else if (number==4){
+        } else if (number == 4) {
             return "The number is equal to 4.";
+        } else if (number < 3) {
+            return "the number is lower than 3.";
         }
-        else if(number <3 ){
-return "the number is lower than 3.";
-}
-return "not found";
-}
+        return numere3si4(number);
+    }
+
+    public String isEligibleToVote(int age) {
+        if (age >= 18) {
+            return "are";
+
+        } else {
+            return "nu are";
+        }
+    }
+
+    public String isNumberEven(int numar) {
+        if (numar % 2 == 0) {
+            return " par.";
+        } else {
+            return "impar.";
+        }
+    }
+
+    public int BiggestNumber(int a, int b, int c) {
+        if (a > b && a > c) {
+            return a;
+        } else if (b > a && b > c) return b;
+        if (a > c && a > b) {
+            return a;
+        } else if (c > a && c > b) {
+            return c;
+        }
+    return BiggestNumber(a, b, c); }
 
 }
