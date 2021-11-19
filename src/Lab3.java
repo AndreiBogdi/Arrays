cfa1ba842cf9461b8af88ae46241281ba29c6d41
Andrei Nica <andreinyca@gmail.com>
2021-11-19 21:40:58 +0200

    TemaArray

--- src/Lab3.java
+++ src/Lab3.java
@@ -23,19 +23,33 @@ public class Lab3 {
         String numar = op.isNumberEven(numarpar);
         int BiggestNumber = op.BiggestNumber(a, b, c);
 
+        int[] myArr = op.populateArray(100);
+        System.out.println("Exercitiul 2: ");
+        op.printArray(myArr);
 
-        System.out.println("Exercitiul 1: \n Rezultatul adunarii 5+8 este "+ (adunare));
-        System.out.println("Rezultatul scaderii 5-8 este "+ (scadere));
-        System.out.println("Rezultatul inmultirii 5*8 este "+ (inmultire));
-        System.out.println("Rezultatul adunarii 5:8 este "+ (impartire));
-        System.out.println("Exercitiul 3: Dintre 5 si 8 mai mare este "+ (commparatie));
-        System.out.println("Exercitiul 4: "+(op.verifyProvidedText(text,text2,text3)));
-        System.out.println("Exercitiul 5: " + (op.verifyText(text4,number)));
-        System.out.println("Exercitiul 6: " +op.numarcomparatie(number));
-        System.out.println("Exercitiul 7 " +op.numere3si4(number));
-        System.out.println("Exercitiul 8:The number is: " +(op.getNumber(number))+"!");
-        System.out.println("Exercitiul 9:Andrei " + (op.isEligibleToVote(17)) +" drept de vot!");
-        System.out.println("Exercitiul 10: " +(numarpar) +" este numar " +(numar));
-        System.out.println("Exercitiul 11: Cel mai mare numar este " +(BiggestNumber));
+        int[] myEvenArr = op.populateEvenArray(100);
+        System.out.println("Exercitiul 3: ");
+        op.printEvenArray(myEvenArr);
+
+        int[] arr = {10,42,5,7};
+        System.out.println("Exercitiul 4: " + op.ArrayAverage(arr));
+
+        String[] array = { "cpu","motherboard", "ssd", "gpu","psu"};
+        System.out.println("Exercitiul 5: "+ op.Array(array));
+
+
+       // System.out.println("Exercitiul 1: \n Rezultatul adunarii 5+8 este "+ (adunare));
+       // System.out.println("Rezultatul scaderii 5-8 este "+ (scadere));
+    //   System.out.println("Rezultatul inmultirii 5*8 este "+ (inmultire));
+        //     System.out.println("Rezultatul adunarii 5:8 este "+ (impartire));
+        //      System.out.println("Exercitiul 3: Dintre 5 si 8 mai mare este "+ (commparatie));
+        //      System.out.println("Exercitiul 4: "+(op.verifyProvidedText(text,text2,text3)));
+        //   System.out.println("Exercitiul 5: " + (op.verifyText(text4,number)));
+        //    System.out.println("Exercitiul 6: " +op.numarcomparatie(number));
+        //   System.out.println("Exercitiul 7 " +op.numere3si4(number));
+        //    System.out.println("Exercitiul 8:The number is: " +(op.getNumber(number))+"!");
+        //   System.out.println("Exercitiul 9:Andrei " + (op.isEligibleToVote(17)) +" drept de vot!");
+        //   System.out.println("Exercitiul 10: " +(numarpar) +" este numar " +(numar));
+        //   System.out.println("Exercitiul 11: Cel mai mare numar este " +(BiggestNumber));
     }
 }
--- src/LogicalOperation.java
+++ src/LogicalOperation.java
@@ -80,4 +80,64 @@ public class LogicalOperation {
         }
     return BiggestNumber(a, b, c); }
 
+    //2.  In clasa LogicalOp, creati o metoda care sa scrie pe un array de 100 de pozitii, valorile de la 1 la 100.
+    // Sa se afiseze progresul in consola pe tot parcursul. Apelati metoda in main() pentru a verifica daca functioneaza.
+    public int[] populateArray(int  max){
+        int[] myArr = new int[max];
+        for(int i=0;i< max;i++){
+            myArr[i] = i+1;
+        }
+        return myArr;
+    }
+
+    // 3. Creati o metoda care primeasca un parametru de tip array de numere intregi gol, si sa il returneze populat cu
+    // toate valorile pare de la 1 la 100. Apelati metoda in main() pentru a verifica daca functioneaza. Atentie,
+    // metoda returneaza un array, deci acesta va trebui surprins si afisat folosind o bucla.
+    public void printArray(int[] array){
+        for(int i=0;i< array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+    public int[] populateEvenArray(int max){
+        int[] myArr = new int[max];
+        for(int i=0;i<max;i++){
+            if(myArr[i] % 2==0){
+                myArr[i]=i+1;
+            }
+        }
+        return myArr;
+    }
+    public void printEvenArray(int[] array){
+        for(int i=0;i < array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+
+    //4. Creati o metoda care sa primeasca un parametru de tip array de numere, si sa fie populat cu valori. Metoda
+    // sa calculeze si sa returneze media numerelor din array. Apelati metoda in main() pentru a verifica daca functioneaza.
+
+    public int ArrayAverage(int[] arr){
+        int sum=0;
+        int ArrayElements=4;
+        for(int value: arr){
+            sum+= value;
+        }
+        return sum/ArrayElements;
+    }
+
+    //5. Creati o metoda care sa primeasca un parametru de tip array de string-uri, populat cu valori, si un parametru
+    // de tip String. Metoda sa verifice daca valoarea string-ului primit se regaseste in array-ul primit,
+    // iar daca da sa returneze true iar daca nu, sa returneze false.  Apelati metoda in main() pentru a verifica daca functioneaza.
+
+    public boolean Array(String[] arr){
+        String ceva="cpu";
+        if(ceva.equals(arr)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+
 }
